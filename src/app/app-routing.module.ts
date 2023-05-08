b12bf5735f95afdca7c4a67e24d4c7ee3114e6ff
Hamilton-Elsik <99698907+Hamilton-Elsik@users.noreply.github.com>
2023-05-07 22:37:49 -0500

    cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BaseLayoutComponent } from './layout/base-layout/base-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home/portafolio', pathMatch: 'full' },
+  { path: 'home', component: BaseLayoutComponent, children:[
+    {
+      path: 'portafolio',
+      loadChildren: () => import('./home/home/home.module').then(m => m.HomeModule)
+    },
+  ]
+},
+  { path: '**', redirectTo: 'home/portafolio', pathMatch: 'full' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
